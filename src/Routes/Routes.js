59cc16ba7b806ec771c57646cebc0f3b771ceb4f
Md. Fazaly Rabby <fazalyrabbi614@gmail.com>
2022-10-25 12:45:42 +0600

    ErrorPage Complete

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "../components/ErrorPage/ErrorPage";
 import Main from "../layout/Main";
 import Blog from "../Pages/Blog/Blog";
 import Home from "../Pages/Home/Home";
@@ -10,6 +11,7 @@ export const routes = createBrowserRouter([
     {
         path: '/',
         element: <Main></Main>,
+        errorElement: <ErrorPage></ErrorPage>,
         children: [
             {
                 path: '/',
---
+++ src/components/ErrorPage/ErrorPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './ErrorPage.css';
+
+const ErrorPage = () => {
+    return (
+        <section className="flex align-items-center mx-auto justify-content-center error">
+            <div className="d-flex align-items-center justify-content-center vh-100">
+                <div className="text-center container">
+                    <h1 className="display-1 fw-bold text-danger">404</h1>
+                    <p className="fs-3"> <span className="text-danger">Opps!</span> Page not found.</p>
+                    <p className="lead">
+                        The page you're looking for doesn't exist.
+                    </p>
+                    <Link to='/' className="btn btn-primary text-decoration-none">Back To Home</Link>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default ErrorPage;
